5547cbe895682b23ca94ce5f3bb9f5aa64e539d5
liuhaiying <liuhaiying_0401@163.com>
2024-01-16 10:38:00 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/main.scss'
+import { Message } from 'element-ui'
 
-Vue.config.productionTip = false;
+Vue.use(ElementUI)
+Vue.prototype.$message = Message
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
